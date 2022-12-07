f6df6fd121389bba522be26a73bbc7b2a32a9aa7
STHobbes <royster.hall@gmail.com>
2022-12-06 19:24:34 -0800

    Documentation improvements.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -45,7 +45,7 @@ public final class Constants extends A05Constants
     // kP for keeping drive at the same orientation
     public static double DRIVE_ORIENTATION_kP = 1.2;
 
-    // for prototype, length and width from center of the wheels, in m (note chassis is 30" square,
+    // for practice, length and width from center of the wheels, in m (note chassis is 30" square,
     // the bolt pattern is 29" square, wheels are 2.75" in from the bolt pattern or centered on the
     // corners of a 23.5"(0.5969m) square.
     // For competition, length and width from center of the wheels, in m (note chassis is 28" square,
@@ -53,8 +53,22 @@ public final class Constants extends A05Constants
     // corners of a 21.5"(0.5461m) square.
     //TODO: set calibration constants here for your relevant robot. rf = right front, lr = left rear, etc
     //TODO: Verify dimensions of your robot
+    /**
+     * The geometry and calibration specific to a swerve drive robot base. We currently have 2 bases, the first being
+     * a <i>prototyping/practice</i> base that should always be in working condition for drive tuning/testing,
+     * calibration, as well as software prototyping. The second is the <i>competition</i> robot that is built for
+     * the yearly competition, and is generally not drivable between the introduction of the competition and a
+     * week or two before the first competition because all the competition-specific appendages are being built
+     * and assembled to it.
+     *
+     * The <i>competition</i> robot is the index 0 entry in the list, so it does not require any changes to the
+     * Roborio. The <i>practice</i> robot requires a jumper on the digital input port 5 that connects the signal
+     * pin to ground. This convention was chosen to minimize the things that could go wrong on the competition robot.
+     *
+     * These settings are loaded into {@link #ROBOT_SETTINGS_LIST} during {@link Robot#robotInit()}
+     */
     public static final A05Constants.RobotSettings[] ROBOT_SETTINGS = {
-            new A05Constants.RobotSettings(0, "Competition", 0.5969, 0.5969, 2.764, 3.559,
+            new A05Constants.RobotSettings(0, "Competition", 0.5461, 0.5461, 2.764, 3.559,
                     4.312, 4.386),
             new A05Constants.RobotSettings(1, "Practice", 0.5969, 0.5969, 5.240, 5.654,
                     0.969, 5.039)
@@ -68,8 +82,8 @@ public final class Constants extends A05Constants
             new A05Constants.DriverSettings("programmer", 0)
     };
 
-    // Connect values to SmartDashboard, if you change the value in smart dashboard it changes the const (speed adjusting etc)
-    // By having two methods, you can optionally add the bounds
+    // Connect values to SmartDashboard, if you change the value in smart dashboard it changes the const
+    // (speed adjusting etc.) By having two methods, you can optionally add the bounds
     /**
      * Initialize value on SmartDashboard for user input, or if already present, return current value.
      *
@@ -78,6 +92,7 @@ public final class Constants extends A05Constants
      *
      * @return The new value that appears on the dashboard.
      */
+    @SuppressWarnings("unused")
     public static double updateConstant(String key, double initValue) {
         // if key already exists, value will be the current value or whatever we just typed in to the dashboard
         // if key doesn't exist yet, value will be set to initValue and added to SmartDashboard
@@ -99,6 +114,7 @@ public final class Constants extends A05Constants
      *
      * @return The new value that appears on the dashboard.
      */
+    @SuppressWarnings("unused")
     public static double updateConstant(String key, double initValue, double lowerBound, double upperBound) {
         // if key already exists, value will be the current value or whatever we just typed in to the dashboard
         // if key doesn't exist yet, value will be set to initValue and added to SmartDashboard
