7f10a5557a8cc46310b006aaaeb8ac8438d2c70b
STHobbes <royster.hall@gmail.com>
2023-02-01 23:30:31 -0700

    Updated to the latest a05annexRobot library. Added the maxSpeedCalibration factor for both the programming and competition robot.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -67,9 +67,9 @@ public final class Constants extends A05Constants
      */
     public static final A05Constants.RobotSettings[] ROBOT_SETTINGS = {
             new A05Constants.RobotSettings(0, "Competition", 0.5461, 0.5461, 2.700, 1.161,
-                    2.703, 2.443),
+                    2.703, 2.443, 1.0954),
             new A05Constants.RobotSettings(1, "Practice", 0.5969, 0.5969, 5.240, 5.654,
-                    0.969, 5.039)
+                    0.969, 5.039, 1.0403)
     };
 
     public static final A05Constants.AutonomousPath[] AUTONOMOUS_PATHS = {
